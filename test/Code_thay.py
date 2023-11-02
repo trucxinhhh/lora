5dce2951f83238649cb41185f857e5f24c9f2bf5
trucxinhhh <93142476+trucxinhhh@users.noreply.github.com>
2023-11-02 11:37:47 +0700

    Update Code_thay.py
    ok

--- test/Code_thay.py
+++ test/Code_thay.py
@@ -1,19 +1,25 @@
 #! /usr/bin/env python3
 import sys
-sys.path.append('/home/pi/Destop/Code_V5/Lab_9_Lora/Demo/Lora_Driver')
+sys.path.append('/home/vinh/Desktop/test/Lora_Driver')
 
-from Lora_Driver.Iot_Driver import myLora
-Lora = myLora(verbose=False)
+from Lora_Driver.IoT_Driver import mylora
+Lora = mylora(verbose=False)
 Lora.debug_on = 0
-Lora.debug_on = 0
+
 
 from time import sleep
 
-Lora.set_freq(444)
+Lora.set_freq(479)
 
 while(1):
     val_sw1 = Lora.read_data(1,4)
-    print(val_sw1)
+    val_sw2 = Lora.read_data(1,5)
+    val_sw3 = Lora.read_data(1,6)
+    print(val_sw1, val_sw2, val_sw3)
 
-    Lora.write_data(1,9,val_sw1)
+    Lora.write_data(1,8,val_sw1)
+    sleep(0.1)
+    Lora.write_data(1,9,val_sw2)
+    sleep(0.1)
+    Lora.write_data(1,10,val_sw3)
     sleep(0.1)
